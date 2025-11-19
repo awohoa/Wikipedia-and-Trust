ff97cc519685ab5248d0ec530b3e146d794d8b64
mollydog25 <71674717+mollydog25@users.noreply.github.com>
2025-11-18 16:58:54 -0800

    Update .week7.py

--- src/.week7.py
+++ src/.week7.py
@@ -7,11 +7,11 @@ s.headers.update({"User-Agent" : "test/0.0"})
 #gets the revision history and sorces count
 params = {"action" : "query", "format" : "json" , "formatversion" : 2, "meta" : "siteinfo", 
           "generator" : "random" , "grnnamespace" : 0, 
-          "prop" : "revisions|extracts|contributors|extlinks|info","ellimit" : "max","rvlimit" : "max", "explaintext" :1, "inprop":"url"}
+          "prop" : "revisions|extracts|contributors|extlinks|info","ellimit" : "max","rvlimit" : "max", "explaintext" :1, "inprop":"url" , "rvdir":"newer"}
 
 outp = s.get("https://en.wikipedia.org/w/api.php",params=params).json()
 print("article " + outp["query"]["pages"][0]["title"])
-print("date of last revision " + (str)((outp["query"]["pages"][0]["revisions"][0]["timestamp"])))
+print("date of first revision " + (str)((outp["query"]["pages"][0]["revisions"][0]["timestamp"])))
 print("amount of revisions " + (str)(len(outp["query"]["pages"][0]["revisions"])))
 print("number of contributors " + (str)(len(outp["query"]["pages"][0]["contributors"])))
 print("number of characters in the text " + (str)(len(outp["query"]["pages"][0]["extract"])))
@@ -24,8 +24,12 @@ print(outp["query"]["pages"][0]["extract"])
 
 
 url = (outp["query"]["pages"][0]["fullurl"]).replace("https://en.wikipedia.org/wiki/","")
-query = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/{url}/monthly/20231001/20231031"
+date = outp["query"]["pages"][0]["revisions"][0]["timestamp"].replace("-","").split("T")[0]
+query = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/user/{url}/monthly/{date}/20251118"
 outb = s.get(query).json()
+
+totalviews = 0
+for view in outb["items"]:
+    totalviews += view["views"]
 print()
-print()
-print("ratio views/revisions " + (str)(outb["items"][0]["views"]/len(outp["query"]["pages"][0]["revisions"])))
+print("ratio views/revisions " + (str)(totalviews/len(outp["query"]["pages"][0]["revisions"])))
